54fa64f7d87036679e2f36fc13fb320a877a8191
Harry Nash <harrywilliamnash@gmail.com>
2020-12-18 14:50:32 +0000

    Make shrek more shrekish

--- main.py
+++ main.py
@@ -3,26 +3,29 @@ import turtle
 
 HALF_SHREK = [
     [0, 0],
-    [90, -15],
+    [100, -10],
     [85, 30],
     [110, 60],
-    [80, 200],
-    [55, 240],
-    [50, 280],
-    [100, 310],
+    [60, 200],
+    [30, 240],
+    [25, 280],
+    [55, 290],
+    [60, 310],
     [30, 290],
     [0, 290],
 ]
 
 HALF_FARQUAAD = [
-    [100, 310],
-    [100, 290],
+    [60, 310],
+    [140, 290],
     [170, 310],
     [200, 310],
-    [200, -25],
+    [200, -20],
 ] + HALF_SHREK[1:-3]
 
 def draw_a_set(x_offset, y_offset, flip_horizontal, flip_vertical):
+    y_offset += 290 if flip_vertical else 0
+    x_offset += 200 if flip_vertical else 0
 
     up()
     goto(x_offset, y_offset)
@@ -48,27 +51,19 @@ def draw_zs(zs, x_offset, y_offset, flip_horizontal, flip_vertical):
     for x, y in zs:
         goto(x_offset + x_multiplier * x, y_offset + y_multiplier * y)
 
-BEGIN_X = -500
-BEGIN_Y = 0
+BEGIN_X = -200
+BEGIN_Y = 300
 
 start_x = BEGIN_X
 start_y = BEGIN_Y
 
-#turtle.bgcolor("red")
-#draw_a_set(x_offset=start_x, y_offset=start_y, flip_horizontal=False, flip_vertical=False)
-#draw_a_set(x_offset=start_x, y_offset=start_y, flip_horizontal=True, flip_vertical=False)
-
-#draw_a_set(x_offset=start_x + 200, y_offset=start_y + 600, flip_horizontal=False, flip_vertical=True)
-#draw_a_set(x_offset=start_x + 200, y_offset=start_y + 600, flip_horizontal=True, flip_vertical=True)
-
-for row in range(2):
-    should_flip_vertical = row % 2 == 0
-    extra = 0 if should_flip_vertical else 200
-    for col in range(2):
-        draw_a_set(x_offset=start_x + extra, y_offset=start_y, flip_horizontal=False, flip_vertical=should_flip_vertical)
-        draw_a_set(x_offset=start_x + extra, y_offset=start_y, flip_horizontal=True, flip_vertical=should_flip_vertical)
+for row in range(4):
+    should_flip_vertical = row % 2 == 1
+    for col in range(4):
+        draw_a_set(x_offset=start_x, y_offset=start_y, flip_horizontal=False, flip_vertical=should_flip_vertical)
+        draw_a_set(x_offset=start_x, y_offset=start_y, flip_horizontal=True, flip_vertical=should_flip_vertical)
         start_x += 400
-    start_y -= 600
+    start_y -= 310 # height of farquaad
     start_x = BEGIN_X
 
 
