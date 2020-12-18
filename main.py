5ab7b40e8cf7a6e196158cc482ffc4b002b11bdf
Harry Nash <harrywilliamnash@gmail.com>
2020-12-18 13:25:43 +0000

    Make them fit together nicer

--- main.py
+++ main.py
@@ -48,15 +48,28 @@ def draw_zs(zs, x_offset, y_offset, flip_horizontal, flip_vertical):
     for x, y in zs:
         goto(x_offset + x_multiplier * x, y_offset + y_multiplier * y)
 
-start_x = 0
-start_y = 0
+BEGIN_X = -500
+BEGIN_Y = 0
+
+start_x = BEGIN_X
+start_y = BEGIN_Y
 
 #turtle.bgcolor("red")
-draw_a_set(x_offset=start_x, y_offset=start_y, flip_horizontal=False, flip_vertical=False)
-draw_a_set(x_offset=start_x, y_offset=start_y, flip_horizontal=True, flip_vertical=False)
+#draw_a_set(x_offset=start_x, y_offset=start_y, flip_horizontal=False, flip_vertical=False)
+#draw_a_set(x_offset=start_x, y_offset=start_y, flip_horizontal=True, flip_vertical=False)
 
-draw_a_set(x_offset=start_x + 200, y_offset=start_y + 600, flip_horizontal=False, flip_vertical=True)
-draw_a_set(x_offset=start_x + 200, y_offset=start_y + 600, flip_horizontal=True, flip_vertical=True)
+#draw_a_set(x_offset=start_x + 200, y_offset=start_y + 600, flip_horizontal=False, flip_vertical=True)
+#draw_a_set(x_offset=start_x + 200, y_offset=start_y + 600, flip_horizontal=True, flip_vertical=True)
+
+for row in range(2):
+    should_flip_vertical = row % 2 == 0
+    extra = 0 if should_flip_vertical else 200
+    for col in range(2):
+        draw_a_set(x_offset=start_x + extra, y_offset=start_y, flip_horizontal=False, flip_vertical=should_flip_vertical)
+        draw_a_set(x_offset=start_x + extra, y_offset=start_y, flip_horizontal=True, flip_vertical=should_flip_vertical)
+        start_x += 400
+    start_y -= 600
+    start_x = BEGIN_X
 
 
 ts = turtle.getscreen()
